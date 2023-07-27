7301f7c509534585eb846db81cf58fdd464314f3
RolandSzentesi <rampageroli@gmail.com>
2023-07-26 23:57:28 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,26 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      name = params[:name]
+      students = Student.where('upper(first_name) = ? OR upper(last_name) = ?', name.upcase, name.upcase)
+    else
+      students = Student.all
+    end
     render json: students
+    
+  #   if params.include?(:name)
+  #     name = params[:name].capitalize()
+  #     students = Student.all.where('first_name = ? OR last_name = ?', name, name)
+  # else
+  #   students = Student.all
+  # end
+  # render json: students
+end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
